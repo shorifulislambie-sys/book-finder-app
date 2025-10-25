37b9522e6e29ff105d15c518e42516aa7e0a1f1d
MD SHORIFUL ISLAM <shorifulislambie-sys@gmail.com>
2025-10-25 16:39:22 +0900

    Fix: Add searchBooks method to BookRepository

--- src/main/java/com/bookfinder/repository/BookRepository.java
+++ src/main/java/com/bookfinder/repository/BookRepository.java
@@ -1,10 +1,17 @@
 package com.bookfinder.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.bookfinder.model.Book;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
+    
+    @Query("SELECT b FROM Book b WHERE b.title LIKE %:keyword% OR b.author LIKE %:keyword%")
+    List<Book> searchBooks(@Param("keyword") String keyword);
 }
