90671b1ff781c1a52c408282023133449ce9cce1
MD SHORIFUL ISLAM <shorifulislambie-sys@gmail.com>
2025-10-25 16:04:39 +0900

    Fix: Add JPA configuration

---
+++ src/main/java/com/bookfinder/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.bookfinder.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaRepositories(basePackages = "com.bookfinder.repository")
+public class JpaConfig {
+}
